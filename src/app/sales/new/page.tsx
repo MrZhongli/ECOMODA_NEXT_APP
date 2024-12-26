b72aaccec86c19539b22f71e046bd3c1297831cb
kenneth chan <david.kachi18@gmail.com>
2024-12-26 14:23:45 -0400

    [short] creating new sales component to sales section

--- src/app/sales/new/page.tsx
+++ src/app/sales/new/page.tsx
@@ -1,9 +1,11 @@
+import NuevoPresupuesto from "./NuevoPresupuesto";
 
 export default async function NewSalePage() {
 
     return (
         <section className="w-full max-h-full p-8">
-            <h2 className="text-3xl text-black font-bold mb-5">esta es la vista de nuevos pedidos</h2>
+            <h2 className="text-3xl text-black font-bold mb-5">Nuevo pedido</h2>
+            <NuevoPresupuesto/>
         </section>
     );
 };
