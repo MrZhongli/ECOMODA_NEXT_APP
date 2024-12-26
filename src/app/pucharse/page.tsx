874e10f1fd7135826078b2b3843e5d0d86f5f947
GaboEPR <elgaboperezr@gmail.com>
2024-12-26 14:25:48 -0400

    pucharse

---
+++ src/app/pucharse/page.tsx
@@ -0,0 +1,8 @@
+export default async function PucharsePage() {
+
+    return (
+        <section className="w-full max-h-full p-8">
+            <h2 className="text-3xl text-black font-bold mb-5">esta es la vista de compra</h2>
+        </section>
+    );
+};
